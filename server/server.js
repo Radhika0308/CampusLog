bba1f429b33a8cf958690648bc6a0f6ae8c48a98
Radhika0308 <radhsiitg@gmail.com>
2025-09-06 01:30:41 +0530

    fix for deployment

--- server/server.js
+++ server/server.js
@@ -7,24 +7,26 @@ import blogRouter from './routes/blogRoutes.js';
 
 const app = express();
 
-const startServer = async () => {
-  await connectDB();
+// Middleware
+app.use(cors());
+app.use(express.json());
 
-  // Middleware
-  app.use(cors());
-  app.use(express.json());
+// Routes
+app.get('/', (req, res) => res.send("API IS WORKING"));
+app.use('/api/admin', adminRouter)
+app.use('/api/blog', blogRouter)
 
-  // Routes
-  app.get('/', (req, res) => res.send("API IS WORKING"));
-  app.use('/api/admin', adminRouter)
-  app.use('/api/blog', blogRouter)
+// For local development
+if (process.env.NODE_ENV !== 'production') {
+  const startServer = async () => {
+    await connectDB();
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+      console.log('Server is running on port ' + PORT);
+    });
+  };
+  startServer();
+}
 
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
-    console.log('Server is running on port ' + PORT);
-  });
-
-};
-startServer();
-
+// For Vercel deployment
 export default app;
