0022db0d386cdb216050d4099a3041ebe1045708
Radhika0308 <radhsiitg@gmail.com>
2025-09-06 01:40:54 +0530

    fixes

--- server/configs/db.js
+++ server/configs/db.js
@@ -4,9 +4,13 @@ import mongoose from "mongoose";
 const connectDB = async() => {
     try{
         mongoose.connection.on('connected',()=> console.log("Database connected"))
-     await mongoose.connect(`${process.env.MONGODB_URI}/CampusLog`)
+        await mongoose.connect(process.env.MONGODB_URI, {
+            dbName: 'CampusLog',
+            serverSelectionTimeoutMS: 5000, // Timeout after 5s instead of 30s
+            socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+        })
     } catch(error){
-      console.log(error.message);
+      console.log("Database connection error:", error.message);
     }
 }
 
